6c10e8bb073eb38d21b076f9530e785091d5cdcc
Renée De Voursney <renee@nird.us>
2013-05-06 16:59:36 -0400

    answers from day 1 pt 2

--- exercises/calculator.rb
+++ exercises/calculator.rb
@@ -7,8 +7,20 @@ class Calculator
     @num_calculations = 0
   end
 
-  def sum(n1,n2)
+  def sum(*n)
     @num_calculations += 1
-    n1+n2
+    total = 0
+    n.flatten.each do |i|
+      total += i
+    end
+    total
+  end
+
+  def multiply *args
+    total = 1
+    args.flatten.each do |i|
+      total *= i
+    end
+    total
   end
 end
