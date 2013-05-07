ae38ebd27a2c10f552025363230e9ff1ca6831e2
Renée De Voursney <renee@nird.us>
2013-05-07 12:21:29 -0400

    more examples and answers

--- examples/vampire.rb
+++ examples/vampire.rb
@@ -1,6 +1,13 @@
 require './monster.rb'
 class Vampire < Monster
+  attr_accessor :bite_vic_count
 	def initialize(noc=true, legs=2, name ="Vampire", vul=[:garlic, :sunlight], dangers=[:bites])
-		super(noc,legs,name,vul,dangers)
+		@bite_vic_count = 0
+    super
 	end
+
+  def bite(human)
+    @bite_vic_count += 1
+    super
+  end
 end
